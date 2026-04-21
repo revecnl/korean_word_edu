5a0808cac5020169ebf6ed7a3231d51d24b34f88
revecnl <82161323+revecnl@users.noreply.github.com>
2026-04-21 17:26:34 +0800

    Create roulette-presets.js

---
+++ assets/js/roulette-presets.js
@@ -0,0 +1,18 @@
+const ROULETTE_STORAGE_KEYS = {
+  state: "eduRouletteSavedStateV2",
+  customPresets: "eduRouletteCustomPresetsV2"
+};
+
+const ROULETTE_DEFAULT_STATE = {
+  leftInput: "아버지, 어머니, 형, 오빠, 누나, 언니, 동생, 나",
+  rightInput: "경찰관, 학생, 공무원, 소방관, 운동선수, 회사원, 선생님, 의사",
+  sentence: ""
+};
+
+const ROULETTE_BUILTIN_PRESETS = {
+  국가: "한국, 중국, 일본, 미국, 베트남, 태국, 말레이시아, 몽골",
+  직업: "학생, 회사원, 의사, 선생님, 경찰관, 소방관, 공무원, 요리사",
+  가족: "아버지, 어머니, 형, 오빠, 누나, 언니, 동생, 할머니",
+  물건: "책, 가방, 휴대전화, 컴퓨터, 시계, 안경, 우산, 의자",
+  장소: "학교, 회사, 집, 도서관, 병원, 식당, 은행, 공원"
+};
